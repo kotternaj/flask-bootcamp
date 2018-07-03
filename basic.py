5bf8058fe26a4ac5df743ea5bd93ddd19141f1fe
Untitled <john.d.reddock@gmail.com>
2018-07-03 17:39:36 -0400

    manual CRUD

--- basic.py
+++ basic.py
@@ -7,16 +7,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK-MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Puppy(db.model):
+class Puppy(db.Model):
     # MANUAL TABLE NAME CHOICE
     __tablename__='woof'
     
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.text)
+    name = db.Column(db.Text)
     age = db.Column(db.Integer)
 
     def __init__(self, name, age):
