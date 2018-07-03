2dbfbc5bd7d8f04aa96912d3eee3da4815184ccd
Untitled <john.d.reddock@gmail.com>
2018-07-03 16:50:20 -0400

    grab input and use in flash alert

--- basic2.py
+++ basic2.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, flash, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'kmykey'
 
 class SimpleForm(FlaskForm):
-    submit = SubmitField("Click Me")
+    submit = SubmitField("Submit")
+    breed = StringField("What breed are you?")
 
 @app.route('/', methods=['GET','POST'])
 def index():
     form = SimpleForm()
 
     if form.validate_on_submit():
-        flash('You just clicked the button!')
+        session['breed'] = form.breed.data
+        # flash('You just clicked the button!')
+        flash(f"You just changed your breed to: {session['breed']}")
 
         return redirect(url_for('index'))
     return render_template('index.html', form=form)
