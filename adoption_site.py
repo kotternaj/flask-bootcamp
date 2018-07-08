bd97748cc971dc007fd65ccf74bac4bab6f4b216
Untitled <john.d.reddock@gmail.com>
2018-07-08 16:01:00 -0400

    home and list.html

--- adoption_site.py
+++ adoption_site.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS' = False]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app,db)
