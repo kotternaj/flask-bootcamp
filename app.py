0dec676c438f3add338ca3fb07fa7c77cb8f6f90
Untitled <john.d.reddock@gmail.com>
2018-07-11 18:39:51 -0400

    user registration view

--- app.py
+++ app.py
@@ -37,7 +37,22 @@ def login():
                 next = url_for('welcome_user')
             return redirect(next)
         
-        return render_template('login.html', form=form)
+     return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registering!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 
 
