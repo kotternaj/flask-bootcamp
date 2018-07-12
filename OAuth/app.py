c9d68206ae7a9fa33fa657bdf9f4de8c479a8459
Untitled <john.d.reddock@gmail.com>
2018-07-12 16:01:33 -0400

    OAuth w/ Google 2

--- OAuth/app.py
+++ OAuth/app.py
@@ -1,14 +1,15 @@
 import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 1
-os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = 1
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
 from flask import Flask, redirect, url_for, render_template
-from flask_dance.contrib.google import make_google_blue_print, google
+from flask_dance.contrib.google import make_google_blueprint, google
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
-blueprint = make_google_blue_print(client_id='', client_secret='',
+blueprint = make_google_blueprint(client_id='183505185385-2c3cnredop1egg8qi7rsjsetss5rvkt6.apps.googleusercontent.com', 
+                                   client_secret='ozFmdS6pDkrZQgCSmsRWhKQj',
                                    offline=True, scope=['profile', 'email'])
 
 app.register_blueprint(blueprint, url_prefix='/login')
@@ -25,7 +26,7 @@ def welcome():
     resp = google.get('/oauth2/v2/userinfo')
     assert resp.ok, resp.text 
     email = resp.json()['email']
-    
+
     return render_template('welcome.html', email=email)
 
 @app.route('/login/google')
@@ -38,3 +39,7 @@ def login():
     email = resp.json()['email']
 
     return render_template('welcome.html', email=email)
+
+if __name__ == '__main__':
+    app.run()
+
